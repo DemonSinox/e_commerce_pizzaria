5548f6bd4ddde3fc80c66c3efe8218a1b7a1e0ae
Lucas_Henrique_Vieira <lucasgamer2005com@gmail.com>
2024-12-06 17:16:31 -0300

    Correção de Procedure para buscar por categoria

--- ScriptBanco/FiltrarPorCategoria.sql
+++ ScriptBanco/FiltrarPorCategoria.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 
-CREATE PROCEDURE FiltrarPorCategoria(
+CREATE or replace PROCEDURE FiltrarPorCategoria(
     IN p_categoria VARCHAR(50)
 )
 BEGIN
@@ -9,17 +9,18 @@ BEGIN
         p.nome, 
         p.descricao, 
         p.preco, 
-        c.nome_categoria
+        c.descricao
     FROM 
         Produtos p
     LEFT JOIN 
         Categoria c ON p.id_categoria = c.id_categoria
     WHERE 
-        c.nome_categoria = p_categoria
+        c.descricao = p_categoria
     ORDER BY 
         p.nome ASC;
 END //
 
 DELIMITER ;
 
-
+-- chamada 
+CALL FiltrarPorCategoria('Pizza');
